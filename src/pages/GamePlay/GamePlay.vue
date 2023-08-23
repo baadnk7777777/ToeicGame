e8770329cc624066a885a4945b2185fb8dee2ba4
Meliodas <banktv76@gmail.com>
2023-08-23 11:02:01 +0700

    Update Gameplay.

--- src/pages/GamePlay/GamePlay.vue
+++ src/pages/GamePlay/GamePlay.vue
@@ -1,21 +1,52 @@
 <template lang="">
   <div class="min-h-[100vh] bg-[#fbf3f3] flex flex-col">
     <div class="flex flex-1 justify-center items-center">
-      <div class="">
-        <div class="flex justify-center mb-4 font-bold text-base">
-          Score: {{ score }}
+      <div v-if="counter.src > 5" class="drop-shadow-md rounded-md">
+        <div
+          class="bg-red-400 text-white font-bold text-base md:text-2xl pl-4 rounded-t-md"
+        >
+          Timeout
         </div>
-        <div class="flex justify-center mb-4">{{ answerData.Thai_Word }}</div>
+        <div class="bg-white px-6 py-6 rounded-b-md">
+          <div class="md:text-3xl text-center mb-2">1 min 0 secs</div>
+          <div class="md:text-xl">
+            Please click "Play Again" for new game. or
+            <div class="">"Back to Home" for back to home page.</div>
+          </div>
+          <div class="flex gap-4 justify-center">
+            <q-btn
+              label="Play Again"
+              outline
+              class="min-w-[100px] lg:w-60 text-center lg:text-2xl mt-4"
+              @click="playAgin()"
+            ></q-btn>
+            <q-btn
+              label="Back to Home"
+              outline
+              class="min-w-[100px] lg:w-60 text-center lg:text-2xl mt-4"
+              @click="playAgin()"
+            ></q-btn>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="counter.src < 5" class="">
+        <div class="flex justify-center mb-4 font-bold text-base lg:text-4xl">
+          Score: {{ score }} | Timer: {{ counter.src }}
+        </div>
+        <div class="flex justify-center mb-4 lg:text-2xl">
+          {{ answerData.Thai_Word }}
+        </div>
         <div class="flex gap-4">
           <q-btn
             outline
-            class="min-w-[100px]"
+            class="min-w-[100px] lg:w-60 text-center lg:text-2xl"
             :label="randomDataFirst.Eng_Word"
             @click="submitAnswer(randomDataFirst.No)"
           ></q-btn>
           <q-btn
             outline
-            class="min-w-[100px]"
+            class="min-w-[100px] lg:w-60 text-center lg:text-2xl"
             :label="randomDataSecond.Eng_Word"
             @click="submitAnswer(randomDataSecond.No)"
           ></q-btn>
@@ -76,12 +107,19 @@ const score = ref(0);
 const fireworkActive = ref(false);
 onMounted(() => {
   wordResponse.value = lawJson;
+  startTimer();
   randomWord();
   setTimeout(() => {
     console.log(anim.value.goToAndPlay(150, true));
     anim.value;
   }, 500);
 });
+
+const playAgin = () => {
+  score.value = 0;
+  counter.value.src = 0;
+  randomWord();
+};
 
 const submitAnswer = (wordNo: number) => {
   console.log('submitAnswer');
@@ -104,6 +142,17 @@ const randomWord = async () => {
   const randomAnswer = Math.floor(Math.random() * 2); // 0 -> 1
   answerData.value =
     randomAnswer === 0 ? randomDataFirst.value : randomDataSecond.value;
+};
+
+const counter = ref({
+  min: 0,
+  src: 0,
+});
+
+const startTimer = () => {
+  setInterval(() => {
+    counter.value.src += 1;
+  }, 1000);
 };
 </script>
 <style lang=""></style>
